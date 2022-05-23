6107a541a0e6f298a11e16103bdd72c2c7e75c95
Vladimir-V <anthrwpos@mail.ru>
2022-05-23 19:56:21 +0300

    плавные замкнутые кривые

--- label_for_paint.cpp
+++ label_for_paint.cpp
@@ -28,15 +28,16 @@ path_point *label_for_paint::getNearestPoint(int mouse_x, int mouse_y, double* d
     return result;
 }
 
+//определяет ближайший отрезок (возвращает итератор на вторую точку этого отрезка)
 std::_List_iterator<path_point> label_for_paint::getNearestLine(int mouse_x, int mouse_y, double *distance)
 {
     double min_dist = -1;
     std::_List_iterator<path_point> result;
     auto it_2 = points.begin();
-    if (it_2 == points.end()) return it_2;
+    if (it_2 == points.end()) return it_2;//если только одна точка - возвращаем итератор в конец списка
     auto it_1 = points.end();
-    --it_1;
-    if (it_1 == it_2) return points.end();
+    --it_1;//предпоследняя точка
+    if (it_1 == it_2) return points.end();//если точек две, возвращаем так-же итератор в конец
     while(it_2 != points.end())
     {
         path_point pp1 = *it_1;
@@ -72,7 +73,6 @@ void label_for_paint::paintDots(QPainter& painter)
         painter.setBrush(brush);
         painter.drawEllipse(*p, 10, 10);
     }
-
 }
 
 //линии
@@ -99,11 +99,27 @@ void label_for_paint::paintLines(QPainter &painter)
     }
 }
 
-void label_for_paint::paintArc(QPainter &painter, const path_point &from, const path_point &to, const double angle, const int pnum)
+/* отрисовка ломанной дуги окружности из одной точки в другую
+ * под заданным углом относительно соединяющей их прямой
+ *
+ * пусть А - первая точка, B - вторая, заданы комплексным числом.
+ * t - вектор из точки А в первую точку дуги
+ * c - единичный вектор поворота который изменяет вектор между точками дуги.
+ *
+ * Тогда B = A + t + ct + c^2t + ... c^n t, где n - число точек ломанной.
+ * Отсюда B - A = t(1 + c + c^2 + ... + c^n)
+ * скобка в правой части - геометрическая прогрессия, так что
+ * B - A = t (1-c^n) / (1-c), откуда t = (Β - Α) * (1 - c) / (1-c^n)
+ *
+ * Из геометрии легко увидеть, что Arg(c) = 2F/n, где F - угол между прямой,
+ * соединяющей точки и касательной к дуге, n - число точек ломанной.
+ */
+void label_for_paint::paintArc(QPainter &painter, const path_point &from, const path_point &to, const int pnum)
 {
+    double angle = to._arc_angle;
     std::complex<double> vec_from_to = {(double) (to.x() - from.x()),(double) (to.y() - from.y())};
-    std::complex<double> single_rotation = std::exp(std::complex<double>{0, 2*angle/pnum});
-    std::complex<double> complete_rotation = std::exp(std::complex<double>{0, 2*angle});
+    std::complex<double> single_rotation = std::exp(std::complex<double>{0, -2*angle/pnum});
+    std::complex<double> complete_rotation = std::exp(std::complex<double>{0, -2*angle});
     constexpr std::complex<double> I {1, 0};
     std::complex<double> step_vector = vec_from_to * (I - single_rotation) / (I - complete_rotation);
     std::complex<double> p1{(double) from.x(), (double) from.y()};
@@ -123,49 +139,99 @@ void label_for_paint::paintArcs(QPainter &painter)
     {
         if (last)
         {
-            paintArc(painter, p, *last, last->_angle);
+            paintArc(painter, p, *last);
         }
         last = &p;
     }
-    if (closed && last) paintArc(painter, *last, points.front(), last->_angle);
+    if (closed && last) paintArc(painter, points.front(), *last);
 }
 
-void label_for_paint::calcAngles()
+//вычисляем углы между прямыми соединяющими точки
+void label_for_paint::calc_lines_angles()
 {
-    path_point* prev_point = nullptr;
-    if (closed)
+    using PPIterator = std::_List_iterator<path_point>;
+    using complex = std::complex<double>;
+    if (points.empty()) return;//элементов нет
+    PPIterator point1 = points.begin();
+    PPIterator point2 = point1;
+    ++point2;
+    if (point2 == points.end()) return;//только 1 элемент
+    complex vector_12 {(double) (point2->x() - point1->x()), (double) (point2->y() - point1->y())};
+    double vector_12_length = std::abs(vector_12);
+    complex vector_12_normalized = vector_12 / vector_12_length;
+    PPIterator point3 = point2;
+    ++point3;
+    while (point3 != points.end())
     {
-        auto last_p_it = points.rbegin();
-        if (last_p_it != points.rend())
+        complex vector_23 {(double) (point3->x() - point2->x()), (double) (point3->y() - point2->y())};
+        double vector_23_length = std::abs(vector_23);
+        complex vector_23_normalized = vector_23 / vector_23_length;
+        point2->_lines_rot = vector_23_normalized / vector_12_normalized;
+        vector_12_normalized = vector_23_normalized;
+        point1 = point2;
+        point2 = point3;
+        ++point3;
+    }
+    point3 = points.begin();
+    for (int more_2_points = 0; more_2_points < 2; ++more_2_points)
+    {
+        complex vector_23 {(double) (point3->x() - point2->x()), (double) (point3->y() - point2->y())};
+        double vector_23_length = std::abs(vector_23);
+        complex vector_23_normalized = vector_23 / vector_23_length;
+        point2->_lines_rot = vector_23_normalized / vector_12_normalized;
+        vector_12_normalized = vector_23_normalized;
+        point1 = point2;
+        point2 = point3;
+        ++point3;
+    }
+}
+
+void label_for_paint::calcArcAngles()
+{
+    //вычисляем углы таким образом, чтобы замкнутая кривая
+    //не содержала изломов. Из решения геометрии существуют
+    //две таких кривых, если число точек нечетно
+    //и для стороны A1A2 многоугольника A1A2A3...An
+    //угол между прямой A1A2 и касательной к дуге соединяющей
+    //A1 с A2 равен +-(F1+F2-F3+F4-F5+F6...-Fn) / 2, где Fn -
+    //угол между прямыми An-1An и AnAn+1.
+    //
+    //если число точек четно, решения либо нет,
+    //либо любая плавная цепочка дуг замыкается без излома
+    calc_lines_angles();
+    if (closed && (points.size() % 2 == 1))
+    {
+        int odd = 1;
+        std::complex<double> angles_sum = 0;
+        auto it = points.begin();
+        if (it != points.end()) angles_sum = it->_lines_rot;
+        ++it;
+        if (it != points.end())
         {
-            prev_point = &*last_p_it;
+            while(it != points.end())
+            {
+                if (odd) angles_sum *= it->_lines_rot;
+                else angles_sum /= it->_lines_rot;
+                ++it;
+                odd = 1 - odd;
+            }
+            points.front()._arc_angle = std::arg(angles_sum) / 2.0;
         }
     }
-    path_point* preprev_point = nullptr;
-    std::complex<double> next_vector;
-    std::complex<double> prev_vector;
-    double next_vector_length;
-    std::complex<double> prev_unit_vector;
-    std::complex<double> next_unit_vector;
+    //вычисляем плавную цепочку дуг от первой точки.
+    //Пусть A, B, C - три точки
+    //V1 - угол между касательной к дуге AB и прямой AB
+    //V2 - угол между касательной к дуге BC и прямой BC
+    //тогда V2 = F - V1, где F - угол между прямыми AB и BC
+    path_point* prev_point = nullptr;
     for (auto& p : points)
     {
         if (prev_point)
         {
-            prev_vector = next_vector;
-            next_vector = {(double) (p.x() - prev_point->x()), (double) (p.y() - prev_point->y())};
-            next_vector_length = std::abs(next_vector);
-            prev_unit_vector = next_unit_vector;
-            next_unit_vector = next_vector / next_vector_length;
+            std::complex<double> old_angle = std::exp(std::complex<double>{0,prev_point->_arc_angle});//единичный вектор направления от предыдущей точки
+            std::complex<double> next_angle = p._lines_rot / old_angle; //единичный вектор направления из вычисляемой точки
+            p._arc_angle = std::arg(next_angle);//записываем угол
         }
-        if (preprev_point)
-        {
-            std::complex<double> rotation = prev_unit_vector / next_unit_vector;
-            //double rotation_magnutude = std::arg(rotation);
-            std::complex<double> old_angle = std::exp(std::complex<double>{0,preprev_point->_angle});
-            std::complex<double> next_angle = rotation / old_angle;
-            prev_point->_angle = std::arg(next_angle);
-        }
-        preprev_point = prev_point;
         prev_point = &p;
     }
 }
@@ -208,7 +274,7 @@ void label_for_paint::mousePressEvent(QMouseEvent *ev)
         }
         chosen = nullptr;
     }
-    calcAngles();
+    calcArcAngles();
     repaint();
 }
 
@@ -216,13 +282,17 @@ void label_for_paint::mouseMoveEvent(QMouseEvent *ev)
 {
     auto mouse_x = ev->x();
     auto mouse_y = ev->y();
+    //ближайшая к курсору точка
     double dist;
     bool need_update = false;
     path_point* nearest = getNearestPoint(mouse_x, mouse_y, &dist);
+
+    //ближайшая к курсору линия
     double ldist;
     auto was_nearest_line = nearest_line;
     nearest_line = getNearestLine(mouse_x, mouse_y, &ldist);
     if (was_nearest_line != nearest_line) need_update = true;
+
     if (dist < 10)//есть точка в районе 10 клеток от мышки - подсветить
     {
         if (nearest != near_cursor) need_update = true;
@@ -233,6 +303,7 @@ void label_for_paint::mouseMoveEvent(QMouseEvent *ev)
         if (near_cursor) need_update = true;
         near_cursor = nullptr;
     }
+
     //есть захваченная точка - двигаем
     if (dragged)
     {
@@ -242,7 +313,7 @@ void label_for_paint::mouseMoveEvent(QMouseEvent *ev)
     }
     if (need_update)
     {
-        calcAngles();
+        calcArcAngles();
         repaint();
     }
 }
@@ -268,6 +339,12 @@ double dot_complex(std::complex<double> left, std::complex<double> right)
     return left.real() * right.real() + left.imag() * right.imag();
 }
 
+
+//расстояние из точки до линии
+//определяется как расстояние до ближайшей точки
+//если перпендикуляр опущенный на прямую соединяющей точки
+//попадает мимо отрезка
+//либо длина этого перпендикуляра в ином случае.
 double label_for_paint::path_line::distance(int x, int y)
 {
     if (p1 && p2)
